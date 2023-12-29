be65c17098b688fbcff39d98f06543a9c565f1e2
K0ftry <ericksepulveda94@hotmail.com>
2023-12-29 13:33:45 -0300

    interface ready

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,28 @@ import './App.css';
 function App() {
   const [count, setCount] = useState(0)
 
+  const handleClick = (id) => {
+    document.getElementById(id).play();
+  }
   return (
       <div id="drum-machine">
         
         <div className="drum-pad-container">
-          <button className='drum-pad'><audio id='Q' className='clip' src=''></audio>Q</button>
-          <button className='drum-pad'>W</button>
-          <button className='drum-pad'>E</button>
-          <button className='drum-pad'>A</button>
-          <button className='drum-pad'>S</button>
-          <button className='drum-pad'>D</button>
-          <button className='drum-pad'>Z</button>
-          <button className='drum-pad'>X</button>
-          <button className='drum-pad'>C</button>
+          <button className='drum-pad pad-first-row' onClick={() => handleClick('Q')} onKeyDown={() => handleKeyDown('Q')}><audio id='Q' className='clip' src='https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'></audio>Q</button>
+          <button className='drum-pad pad-first-row' onClick={() => handleClick('W')}><audio id='W' className='clip' src='https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'></audio>W</button>
+          <button className='drum-pad pad-first-row' onClick={() => handleClick('E')}><audio id='E' className='clip' src='https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'></audio>E</button>
+          <button className='drum-pad pad-second-row' onClick={() => handleClick('A')}><audio id='A' className='clip' src='https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'></audio>A</button>
+          <button className='drum-pad pad-second-row' onClick={() => handleClick('S')}><audio id='S' className='clip' src='https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'></audio>S</button>
+          <button className='drum-pad pad-second-row' onClick={() => handleClick('D')}><audio id='D' className='clip' src='https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'></audio>D</button>
+          <button className='drum-pad pad-third-row' onClick={() => handleClick('Z')}><audio id='Z' className='clip' src='https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'></audio>Z</button>
+          <button className='drum-pad pad-third-row' onClick={() => handleClick('X')}><audio id='X' className='clip' src='https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'></audio>X</button>
+          <button className='drum-pad pad-third-row' onClick={() => handleClick('C')}><audio id='C' className='clip' src='https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'></audio>C</button>
         </div>
         <div className="controls">
-          <label class="switch">
-            Power
-            <input type="checkbox" />
-            <span class="slider"></span>
-          </label>
+          <button className='power'>Power</button>
           <div id="display"></div>
           <input type="range" id='volume' name='volume' min={0} max={11} />
-          <label class="switch">
-            Bank
-            <input type="checkbox" />
-            <span class="slider"></span>
-          </label>
+          <button className='bank'>Bank</button>
         </div>
       </div>
   )
